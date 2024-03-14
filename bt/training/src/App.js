dfaa7a5267c894714ff73b042e27f3f41efc845c
Kienroro <kiennguyen@Kiens-MacBook-Pro.local>
2024-03-14 22:58:40 +0700

    update bt SPM

---
+++ bt/training/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='container m-auto'>
+       <div className="title text-black  ">
+        <h1  className='text-5xl  mb-3  text-center '> Bài Tập Css Cho Text </h1>
+        <p className="sub-title text-xs mb-5 font-bold ">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum et aliquam sapiente animi
+            aspernatur nihil vel ullam dolore
+            assumenda consectetur? </p>
+    </div>
+
+    <div className="Heading_1 ">
+        <h1 className='text-2xl font-bold text-black mb-5 '> Heading 1 </h1>
+        <p className='text-base  my-2.5'> 
+            <span className="important font-bold">Lorem ipsum </span>
+            dolor sit amet consectetur adipisicing elit. Sequi, porro. Voluptatem numquam quisquam aperiam vel
+            aspernatur, nobis
+            ducimus eligendi quia adipisci quas, maiores nemo magnam sunt error recusandae eius cumque porro dolorum
+            ipsa, eos delectus modi
+            quos minus odio. Unde ab rerum fugit magni illo libero dicta, dolor beatae assumenda.
+        </p>
+
+        <p className='text-base my-2.5'>Omnis provident placeat, dolorum ab eveniet expedita quod! Sed quibusdam quas omnis suscipit itaque error,
+            quaerat rerum voluptates
+            assumenda velit minus perferendis impedit debitis aperiam eveniet adipisci distinctio obcaecati ipsam
+            deserunt, doloremque, quae
+            veritatis quisquam commodi! Aperiam eos nobis cupiditate perspiciatis ipsum nostrum iste, eius illum ipsam
+            repudiandae nihil totam? </p>
+
+    </div>
+
+
+
+    <div className="Heading_2 ">
+        <h1 className='text-3xl font-bold text-black mb-5'> HEADING 2</h1>
+        <div className="text_1 ">
+            <span className="important text-base  my-2.5  font-bold line-through ">Lorem ipsum,</span>
+            dolor sit amet consectetur adipisicing elit. Hic molestias porro aliquam! Ut harum natus repellat aspernatur
+            iusto
+            voluptatibus perspiciatis voluptatum cum? Nulla officiis unde temporibus amet reiciendis facilis odio rem,
+            maiores inventore soluta?
+            Tempore veniam, voluptates quam id fugiat saepe sapiente. Numquam voluptates dolor quis commodi saepe error,
+            omnis rem, nihil
+            consequuntur quisquam minima deleniti consequatur maiores mollitia labore, adipisci pariatur. Ad quidem
+            pariatur quia ducimus
+            deserunt laboriosam odio dolorem quasi cum dicta? Veritatis delectus dolores autem expedita necessitatibus
+            provident non aspernatur,
+            unde odio aperiam minus aut nobis officia dolorum magnam nam ipsum deserunt distinctio aliquid debitis
+            eaque? Aperiam.
+        </div>
+
+    </div>
+    <div className="author">
+        <h2 className='text-xs float-right text-pink-200'> Phan Văn Cương-Fouder Unitop </h2>
+    </div> 
+
+    </div>
+  );
+}
+
+export default App;
